d068f3aee13c22fe159ecb6c34fbcbf1616e9f77
chencm <chencm@qingcigame.com>
2023-01-09 20:43:27 +0800

    测试

---
+++ test/test.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../src/Path.php';
+
+use Wjoj\Tool\Path;
+
+
+
+$path = new Path('http://localhost/cde/cdd');
+$path2 = $path->new('/test.php');
+echo $path;
+echo $path->isDir() . "\n";
+echo $path->isFile() . "\n";
+echo $path2  . "\n";
+echo $path2->isFile() . "\n";
+echo $path->newAdd('/testc.php') . "\n";
